61b4df3417e11878abaa98809004addf6ccea6e4
ryo0705s <inspire.sasaki15@gmail.com>
2022-03-06 19:00:48 +0900

    データ構造を変えないとtodo事のステータスは変更できないのでその前にコミット

--- app.vue
+++ app.vue
@@ -2,16 +2,19 @@
   <div>
     <input v-model="todo" />
     <button @click="add">追加</button>
-    <!-- <ul>
-      <li v-for="(status, id) in statusList" :key="id" class="status">
-        <button @click="checkStatus(id)">{{ status }}</button>
-      </li>
-    </ul>
-    <br /> -->
     <ul>
       <li v-for="(todo, index) in todos" :key="index">
         {{ todo }}
-        <button @click="checkStatus(index)">{{ status }}</button>
+        <button
+          class="status"
+          @click="checkStatus(index)"
+          v-if="index === currentIndex"
+        >
+          {{ status }}
+        </button>
+        <button class="status" @click="checkStatus(index)" v-else>
+          未着手
+        </button>
         <button @click="remove(index)">削除</button>
         <button @click="edit(index)">編集</button>
       </li>
@@ -29,7 +32,7 @@ export default {
     return {
       statusList: ["着手中", "完遂", "未着手"],
       status: "未着手",
-      currentStatus: "",
+      currentIndex: "",
       todo: "",
       todos: [],
       newTodo: "",
@@ -54,12 +57,19 @@ export default {
       this.todos.splice(this.editTarget, 1, this.newTodo);
       this.target = false;
     },
-    checkStatus() {
+    checkStatus(index) {
       let current = this.status;
       this.status = this.statusList.splice(0, 1).join("");
       this.statusList.push(this.status);
+      this.currentIndex = index;
     },
   },
+  // watch: {
+  //   editTarget(newVal, oldVal) {
+  //     // return newVal;
+  //     console.log(newVal, oldVal);
+  //   },
+  // },
 };
 </script>
 <style scoped>
@@ -67,6 +77,6 @@ li {
   list-style: none;
 }
 .status {
-  float: left;
+  background-color: #aae7aa;
 }
 </style>
